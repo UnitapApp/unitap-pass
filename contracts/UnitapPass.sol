1643ac21139e6430e4861df6718e3610b95c5d7b
Sina <snparvizi75@gmail.com>
2022-12-04 14:54:11 +0330

    set baseURI

--- contracts/UnitapPass.sol
+++ contracts/UnitapPass.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract UnitapPass is ERC721, ERC721URIStorage, AccessControl {
     using Counters for Counters.Counter;
@@ -12,19 +13,18 @@ contract UnitapPass is ERC721, ERC721URIStorage, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
+    string public baseURI;
+
     constructor() ERC721("UnitapPass", "UNTP") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri)
-        public
-        onlyRole(MINTER_ROLE)
-    {
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _setTokenURI(tokenId, Strings.toString(tokenId));
     }
 
     // The following functions are overrides required by Solidity.
@@ -43,6 +43,20 @@ contract UnitapPass is ERC721, ERC721URIStorage, AccessControl {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    event SetBaseURI(string baseURI);
+
+    function setBaseURI(string memory baseURI_)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        baseURI = baseURI_;
+        emit SetBaseURI(baseURI);
     }
 
     function supportsInterface(bytes4 interfaceId)
